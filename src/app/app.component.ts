9b747a946d6f204134fcd77e15c80ff3681d1d55
Fred <frederico.d.silva@outlook.com>
2021-01-17 10:35:26 -0300

    insert diretives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  photos = [
+    {
+      url : 'https://d168rbuicf8uyi.cloudfront.net/wp-content/uploads/2019/06/13145802/sonhar-com-leao-1024x649.jpg',
+      description : 'Lion'
+    }
+  ]
 
 }
