bf676a3fed10e8aec77cff4033a83542c7bfed9d
Fred <frederico.d.silva@outlook.com>
2021-01-24 18:44:39 -0300

    insert pipe and resolve

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoComponent } from './photos/photo/photo.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list-resolver';
 
 const routes: Routes = [
-  { 'path': 'users/:username', component: PhotoListComponent },
+  {
+    'path': 'users/:username',
+    component: PhotoListComponent,
+    resolve:{
+      photos: PhotoListResolver
+    }
+
+  },
   { 'path': 'p/add', component: PhotoComponent },
   { 'path': '**', component: NotFoundComponent },
 ];
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,26 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
 export class PhotoListComponent implements OnInit {
 
   photos: any[] = [];
+  filter: string = '';
 
   constructor(
-    private photoService: PhotoService,
-    private  activitedRoute: ActivatedRoute    
-    ) { }
+    private activitedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-    const username = this.activitedRoute.snapshot.params.username;
-    this.photoService
-      .listFromUser(username+'/photos')
-      .subscribe(photos => this.photos = photos);
+    this.photos = this.activitedRoute.snapshot.data
   }
 
 }
