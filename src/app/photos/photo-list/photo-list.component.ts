9fa4f4946d2b2076ca8fec1dd9384397b04ed503
Fred <frederico.d.silva@outlook.com>
2021-01-25 21:27:32 -0300

    implement load-button

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
+import { PhotoService } from '../photo/photo.service';
+
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
@@ -13,10 +15,15 @@ export class PhotoListComponent implements OnInit {
   photos: any[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
-  constructor(private activitedRoute: ActivatedRoute) { }
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
+
+  constructor(private activitedRoute: ActivatedRoute, private photoService: PhotoService) { }
 
   ngOnInit(): void {
     this.photos = this.activitedRoute.snapshot.data.photos;
+    this.userName = this.activitedRoute.snapshot.params.username;
     this.debounce
       .pipe(debounceTime(300))
       .subscribe(filter => this.filter = filter);
@@ -24,5 +31,14 @@ export class PhotoListComponent implements OnInit {
 
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos =  this.photos.concat(photos);
+        if(!photos.length)this.hasMore = false;
+      })
   }
 }
